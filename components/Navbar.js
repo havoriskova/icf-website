cf4203a9e3668e964b43461fc75a05f665cb8d06
havoriskova <ha.voriskova@gmail.com>
2023-02-09 21:27:56 +0100

    added withRouter for tracking the active nav li

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,32 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from './navbar.module.css';
+import {withRouter} from "next/router";
 
 
-export default function Navbar() {
+function Navbar({router}) {
 
     return(
         <div>
             
             <ul>
-                <li><Link href='/'><Image alt='logo' src='/icf_logo.ico.png'
+                <li className={router.pathname === '/' ?  styles.active : ''}><Link href='/'><Image alt='logo' src='/icf_logo.ico.png'
                 width={70} height={55}/></Link></li>
 
-                <li className={styles.navbarTest}><Link href='/get-involved'>Get involved</Link></li>
-                <li><Link href='/about'>About</Link>
+                <li className={router.pathname === '/get-involved' ? styles.active : ''}><Link href='/get-involved'>Get involved</Link></li>
+                <li className={router.pathname === '/about' ? styles.active : ''}><Link href='/about'>About</Link>
                     <ul>
-                        <li><Link href='/about/team'>Team</Link></li>
-                        <li><Link href='/about/our-work'>Our Work</Link></li>
-                        <li><Link href='/about/contact'>Contact</Link></li>
+                        <li className={router.pathname === '/about/team' ?  styles.active : ''}><Link href='/about/team'>Team</Link></li>
+                        <li className={router.pathname === '/about/our-work' ?  styles.active : ''}><Link href='/about/our-work'>Our Work</Link></li>
+                        <li className={router.pathname === '/about/contact' ?  styles.active : ''}><Link href='/about/contact'>Contact</Link></li>
                     </ul>
                 </li>
-                <li><Link href='/news'>News</Link></li>
-                <li><Link href='/shop'>Impact Shop</Link></li>
-                <li><Link href='/donate'>Donate</Link></li>
+                <li className={router.pathname === '/news' ?  styles.active : ''}><Link href='/news'>News</Link></li>
+                <li className={router.pathname === '/shop' ?  styles.active : ''}><Link href='/shop'>Impact Shop</Link></li>
+                <li className={router.pathname === '/donate' ?  styles.active : ''}><Link href='/donate'>Donate</Link></li>
             </ul>
         </div>
     )
 }
+
+export default withRouter(Navbar);
