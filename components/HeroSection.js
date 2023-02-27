229bf0aaf70f34ca4006f320fa7f0dc71684103c
havoriskova <ha.voriskova@gmail.com>
2023-02-27 20:27:38 +0100

    update of header on hero section

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -7,9 +7,11 @@ export default function HeroSection() {
         <div className={heroStyles.containerHero}>
             <div className={heroStyles.headerContainer}>
                 <h1 className={heroStyles.header}>Inspirational mission statement on what drives ICF.</h1>
-                <h2 className={heroStyles.subHeader}>Lorem ipsum </h2>
-                <button className={heroStyles.buttonHero}>Action 1</button>
-                <button className={heroStyles.buttonHeroTwo}>Action 2</button>
+                <h2 className={heroStyles.subHeader}>Supporting text on what we are trying to achieve with our organization, why it’s important what we do, why we need the support of donors etc. etc.</h2>
+                <div className={heroStyles.buttons}>
+                    <button className={heroStyles.buttonHero}>Take action</button>
+                    <button className={heroStyles.buttonHeroTwo}>Learn more</button>
+                </div>
             </div>
             <video className={heroStyles.heroVideo} src='../videos/lion-placeholder.mp4' autoPlay loop muted />
             
