7a6e1e6f1af4c62829e8110f505418bb7587dc51
havoriskova <ha.voriskova@gmail.com>
2023-02-28 00:02:52 +0100

    added copy function + notification about copy on footer, email

--- components/Footer.js
+++ components/Footer.js
@@ -13,43 +13,42 @@ export default function Footer() {
                         <img alt='logo-fcb' src='../images/Facebook.png' />
                         <img alt='logo-insta' src='../images/Instagram.png' />
                         <img alt='logo-twitter' src='../images/Twitter.png' />
-
                     </p>
-                    
                 </div>
-            <div className={styles.footerBottomGrid}>
-                <div className={styles.footerBottomGridChild}>
-                    <img alt='logo' src='/icf_logo.ico.png'/>
-                </div>
-                <div className={styles.footerBottomGridChild}>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th colSpan="2">Get in touch</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr className={styles.row}>
-                                <td><img alt='logo-place' src='../images/Place.png' /></td>
-                                <td>Cattenhagestraat 211411 CR<br/>
-                                    Naarden, Nederland</td>
-                            </tr>
-                            <tr className={styles.row}>
-                                <td><img alt='logo-letter' src='../images/Letter.png' /></td>
-                                <td>hello@icf.earth</td> {/* kdyz to zmacknes, tak se ti to zkopiruje*/}
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div className={styles.footerBottomGridChild}>
+                <div className={styles.footerBottomGrid}>
+                    <div className={styles.footerBottomGridChild}>
+                        <img alt='logo' src='/icf_logo.ico.png'/>
+                    </div>
+                    <div className={styles.footerBottomGridChild}>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th colSpan="2">Get in touch</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className={styles.row}>
+                                    <td><img alt='logo-place' src='../images/Place.png' /></td>
+                                    <td>Cattenhagestraat 211411 CR<br/>
+                                        Naarden, Nederland</td>
+                                </tr>
+                                <tr className={styles.row}>
+                                    <td><img alt='logo-letter' src='../images/Letter.png' /></td>
+                                    <td className={styles.copyEmail} onClick={() => {navigator.clipboard.writeText("hello@icf.earth"); let notification = document.querySelector('#copyEmail'); notification.classList.add(`${styles.active}`); setTimeout(() => {notification.classList.remove(`${styles.active}`)}, 2000); }}> hello@icf.earth</td> 
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className={styles.footerBottomGridChild}>
                     Sitemap?
+                    </div>
                 </div>
+                <div className={styles.termsOfUse}>
+                        <p>Terms of use</p>
+                        <p>Privacy policy</p>
+                </div>
+                <div className={styles.copyEmailNotification} id='copyEmail' >"hello@icf.earth" copied to your clipboard</div>
             </div>
-            <div className={styles.termsOfUse}>
-                    <p>Terms of use</p>
-                    <p>Privacy policy</p>
-            </div>
-        </div>
         </div>
         
     )
