4e007a7dcbe4d7a6fe10c61d9156a15ec1cca70c
havoriskova <ha.voriskova@gmail.com>
2023-03-02 11:47:00 +0100

    fixed typo on font - now it is Peace Sans properly implemented

---
+++ pages/donate/elephants.js
@@ -0,0 +1,22 @@
+import Layout from '../../components/layout.js';
+import DonorBox from '../../components/DonorBox.js';
+
+
+export default function Elephants() {
+
+    return (
+        <Layout>
+            <div>
+                <p className='firstComponentOnPage'>Animal Donate content. </p>
+                <p>Into this page you will get through Link from the MAP and from social media.</p>
+                <p> </p>
+        
+               
+
+                
+
+                <DonorBox />
+            </div>
+        </Layout>
+    )
+}
