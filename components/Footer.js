0b2d3260b97de950051309797bb64f9e04ea0d9d
havoriskova <ha.voriskova@gmail.com>
2023-03-06 18:50:59 +0100

    update of footer

--- components/Footer.js
+++ components/Footer.js
@@ -8,22 +8,40 @@ export default function Footer() {
         <div className={`${styles.footerTest} ${styles.footer}`}>
            <div className={styles.containerContent}>
                 <div className={styles.bottomBorder}>
-                    <p className={`${styles.center} kumbhSansSemiBold`}>Follow us</p>
+                    <h6 className={styles.center}>Follow us</h6>
                     <p className={`${styles.center} ${styles.socials}`}>
                         <a href=''  target='_blank'><img alt='logo-fcb' src='../images/Facebook.png' /></a>
                         <a href='https://www.instagram.com/icf.earth/' target='_blank'><img alt='logo-insta' src='../images/Instagram.png' /></a>
                         <a href='https://twitter.com/icf_earth'  target='_blank'><img alt='logo-twitter' src='../images/Twitter.png' /></a>
                     </p>
                 </div>
+
                 <div className={styles.footerBottomGrid}>
                     <div className={styles.footerBottomGridChild}>
                         <img alt='logo' src='/icf_logo.ico.png'/>
                     </div>
-                    <div className={styles.footerBottomGridChild}>
+                    <div className={`${styles.footerBottomGridChild} ${styles.sitemap}`}>
+                        <h6>Get involved</h6>
+                        <p>Donate</p>
+                        <p>Donate using crypto</p>
+                        <h6>Stories</h6>
+                    </div>
+                    <div className={`${styles.footerBottomGridChild} ${styles.sitemap}`}>
+                        <h6>About ICF</h6>
+                        <p>Meet the team</p>
+                        <p>Our work</p>
+                        <p>Contact us</p>
+                        <h6>Impact shop</h6>
+                    </div>
+                    <div className={`${styles.footerBottomGridChild} ${styles.sitemap}`}>
+                        <h6>Partners</h6>
+                    </div>
+                    
+                    <div className={`${styles.footerBottomGridChild} ${styles.getInTouch}`}>
                         <table>
                             <thead>
                                 <tr>
-                                    <th colSpan="2" className='kumbhSansSemiBold'>Get in touch</th>
+                                    <th colSpan="2"><h6>Get in touch</h6></th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -38,14 +56,18 @@ export default function Footer() {
                                 </tr>
                             </tbody>
                         </table>
+                        <h6>Terms of use</h6>
+                        <h6>Privacy policy</h6>
                     </div>
-                    <div className={styles.footerBottomGridChild}>
-                    Sitemap?
-                    </div>
+                   
                 </div>
-                <div className={`${styles.termsOfUse} kumbhSansSemiBold`}>
-                        <p>Terms of use</p>
-                        <p>Privacy policy</p>
+
+                <div className={styles.ourAccreditation}>
+                        <h6>Our accreditaion</h6>
+                        <div className={styles.ourAccreditationFlex}>
+                            <span><img alt='anbi' src='./images/anbi.png'/></span>
+                            <span><img alt='ngo' src='./images/ngo.png'/></span>
+                        </div>
                 </div>
                 <div className={styles.copyEmailNotification} id='copyEmail' >"hello@icf.earth" copied to your clipboard</div>
             </div>
