76510f230052eba4dbad17d301c91ddc6c513c62
havoriskova <ha.voriskova@gmail.com>
2023-03-26 00:01:51 +0100

    fixed font of buttons on hero section

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -9,8 +9,8 @@ export default function HeroSection() {
                 <h1 className={heroStyles.header}>Inspirational mission statement on what drives ICF.</h1>
                 <h2 className={heroStyles.subHeader}>Supporting text on what we are trying to achieve with our organization, why it’s important what we do, why we need the support of donors etc. etc.</h2>
                 <div className={heroStyles.buttons}>
-                    <button className={heroStyles.buttonHero}>Take action</button>
-                    <button className={heroStyles.buttonHeroTwo}>Learn more</button>
+                    <button className={`${heroStyles.buttonHero} kumbhSansSemiBold`}>Take action</button>
+                    <button className={`${heroStyles.buttonHeroTwo} kumbhSansSemiBold`}>Learn more</button>
                 </div>
             </div>
             <video className={heroStyles.heroVideo} src='../videos/lion-placeholder.mp4' autoPlay loop muted />
