1d47f76086e0a8b8b33b9d18011bf7eee5991e04
havoriskova <ha.voriskova@gmail.com>
2023-03-31 15:05:21 +0200

    added Our Partners page

--- pages/partners.js
+++ pages/partners.js
@@ -1,5 +1,6 @@
 import Layout from '../components/layout.js';
-
+import styles from './partners.module.css';
+import Link from 'next/link';
 
 export default function Partners() {
 
@@ -9,8 +10,74 @@ export default function Partners() {
 
         
         
-            <div className='firstComponentOnPage'>Partners content</div>
+                <div className='firstComponentOnPage'>
+                    <div  className="headingForSections centeredWithLeave">
+                                <span className="headingLeave orangeLeave centerLeave"></span>
+                                <h3>Our Partners<span className="headingRedDot">.</span></h3>
+                    </div>
+                </div>
+
+                
+                <div className='contentWidth'>
+
+                    <div className="headingForSections greenMiddleHeading">
+                        <h3>Subtitle (type of partner)</h3>
+                    </div>
+
+
+                    <div className={styles.flexParentForLogos}>
+                        <div className={styles.logoParent}>
+                            <div>Partner logo linking to partner website</div>
+                        </div>
+
+                        <div className={styles.logoParent}>
+                            <div>Partner logo linking to partner website</div>
+                        </div>
+
+                        <div className={styles.logoParent}>
+                            <div>Partner logo linking to partner website</div>
+                        </div>
+
+                        <div className={styles.logoParent}>
+                            <div>Partner logo linking to partner website</div>
+                        </div>
+
+                    </div>
+
+                    <div className="headingForSections greenMiddleHeading">
+                        <h3>Subtitle (type of partner)</h3>
+                    </div>
+
+                    <div  className={styles.flexParentForLogos}>
+                        <div className={styles.logoParent}>
+                            <div>Partner logo linking to partner website</div>
+                        </div>
+
+                        <div className={styles.logoParent}>
+                            <div>Partner logo linking to partner website</div>
+                        </div>
+
+                        <div className={styles.logoParent}>
+                            <div>Partner logo linking to partner website</div>
+                        </div>
+
+                        <div className={styles.logoParent}>
+                            <div>Partner logo linking to partner website</div>
+                        </div>
+                    </div>
+                    
+                </div>
+
+
+
+
+                    <div className='articleMargin centeredText lastComponentOnPage'>
+                        <h4 className='kumbhSansSemiBold'>Want to partner with ICF? Contact us below</h4>
+                        <button className='readMore accentColor'><Link href=''>Become a partner</Link></button>
+                    </div>
+
             </div>
+
         </Layout>
     )
 }
