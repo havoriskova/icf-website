bb3834de773d46ed86db3a17aa73c6f41459f6b1
havoriskova <ha.voriskova@gmail.com>
2023-04-06 23:11:06 +0200

    update blog post + change text-align to left according to wireframes

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -19,10 +19,14 @@ export default function BlogPost() {
               <p className='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem recusandae ducimus voluptatem deleniti molestias nostrum molestiae, modi asperiores aspernatur dolor deserunt sed! Sunt iure earum iusto ducimus ipsa, officiis fuga?
               Suscipit perspiciatis similique quos ea officiis soluta corporis corrupti iste quisquam cupiditate tempora ullam, eaque, debitis saepe. Eveniet possimus debitis doloribus blanditiis, fuga laudantium pariatur magni animi modi libero. Minus?</p>
 
-              <p className={`${styles.lastParagraph} paragraph`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis quia, voluptate ratione laboriosam natus quaerat aliquam fuga? Dolor temporibus nemo provident, expedita eaque sunt, iusto eveniet error at minus quas.</p>
-        
-              <div className={styles.imageContainerHalfWidth}>
-                <img alt='placeholder' src='/images/homepage_3.jpg'/>
+
+              <div className={styles.lastParagraph}>
+                    <p className='paragraph'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis voluptatem placeat veritatis ex natus quae commodi sequi? Enim, consequuntur. Nulla repellat consequatur hic consequuntur laboriosam cupiditate quasi quos tempora error. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis quia, voluptate ratione laboriosam natus quaerat aliquam fuga? Dolor temporibus nemo provident, expedita eaque sunt, iusto eveniet error at minus quas.</p>
+                
+                    <div className={styles.imageContainerHalfWidth}>
+                        <img alt='placeholder' src='/images/homepage_3.jpg'/>
+                    </div>
+            
               </div>
             
             </div>
