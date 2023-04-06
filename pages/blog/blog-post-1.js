44256f844ff40425a72d7479b21f81935631c1bc
havoriskova <ha.voriskova@gmail.com>
2023-04-06 15:18:23 +0200

    added new pages

---
+++ pages/blog/blog-post-1.js
@@ -0,0 +1,10 @@
+export default function BlogPostOne() {
+
+    return (
+        <Layout pageTitle='ICF About'>
+            <div className='container'>
+                
+        </div>
+        </Layout>
+    )
+}
---
+++ pages/blog/blog-post-2.js
@@ -0,0 +1,10 @@
+export default function BlogPostTwo() {
+
+    return (
+        <Layout pageTitle='ICF About'>
+            <div className='container'>
+                
+            </div>
+        </Layout>
+    )
+}
