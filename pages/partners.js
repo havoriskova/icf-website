8ceb0d7adb7645f40bcdfd8b080fde4052a7ff99
havoriskova <ha.voriskova@gmail.com>
2023-04-08 09:27:24 +0200

    partners logo added

--- pages/partners.js
+++ pages/partners.js
@@ -1,6 +1,7 @@
 import Layout from '../components/layout.js';
 import styles from './partners.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Partners() {
 
@@ -21,48 +22,53 @@ export default function Partners() {
                 <div className='contentWidth'>
 
                     <div className="headerMd greenMiddleHeading">
-                        <h3>Subtitle (type of partner)</h3>
+                        <h3>Funding partners</h3>
                     </div>
 
 
                     <div className={styles.flexParentForLogos}>
                         <div className={styles.logoParent}>
-                            <div>Partner logo linking to partner website</div>
+                            <div><Image alt='logo' src='/images/partners/funding1.png' width={100} height={50} /></div>
                         </div>
 
                         <div className={styles.logoParent}>
-                            <div>Partner logo linking to partner website</div>
+                            <div><Image alt='logo' src='/images/partners/fuding2.jpg' width={100} height={50} /></div>
                         </div>
 
                         <div className={styles.logoParent}>
-                            <div>Partner logo linking to partner website</div>
+                            <div><Image alt='logo' src='/images/partners/funding3.jpg' width={100} height={50} /></div>
                         </div>
 
                         <div className={styles.logoParent}>
-                            <div>Partner logo linking to partner website</div>
+                            <div><Image alt='logo' src='/images/partners/funding4.jpg' className={styles.invertColor} width={100} height={50} /></div>
+                        </div>
+
+                        <div className={styles.logoParent}>
+                            <div><Image alt='logo' src='/images/partners/funding5.jpg' width={100} height={50} /></div>
                         </div>
 
                     </div>
 
                     <div className="headerMd greenMiddleHeading">
-                        <h3>Subtitle (type of partner)</h3>
+                        <h3>Conservation partners</h3>
                     </div>
 
                     <div  className={styles.flexParentForLogos}>
                         <div className={styles.logoParent}>
-                            <div>Partner logo linking to partner website</div>
+                            <div><Image alt='logo' src='/images/partners/conservation1.jpg' width={100} height={50} /></div>
                         </div>
 
                         <div className={styles.logoParent}>
-                            <div>Partner logo linking to partner website</div>
+                            <div><Image alt='logo' src='/images/partners/conservation2.png' width={100} height={50} /></div>
                         </div>
+                    </div>
 
+                    <div className="headerMd greenMiddleHeading">
+                        <h3>Technical partners</h3>
+                    </div>
+                    <div  className={styles.flexParentForLogos}>
                         <div className={styles.logoParent}>
-                            <div>Partner logo linking to partner website</div>
-                        </div>
-
-                        <div className={styles.logoParent}>
-                            <div>Partner logo linking to partner website</div>
+                            <div><Image alt='logo' src='/images/partners/technical1.jpg' width={100} height={50} /></div>
                         </div>
                     </div>
                     
